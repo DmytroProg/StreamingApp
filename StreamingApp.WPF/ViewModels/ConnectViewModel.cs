ae514311697b8efeeac9e89f874d095010fb9592
RostyslavG <rostyslav.gritsyshen@gmail.com>
2023-12-19 15:45:23 +0200

    Added new controls, naming, small optimization of styles.

---
+++ StreamingApp.WPF/ViewModels/ConnectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StreamingApp.WPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.WPF.ViewModels
+{
+    internal class ConnectViewModel : ViewModelBase
+    {
+        public ConnectViewModel() { }
+    }
+}
---
+++ StreamingApp.WPF/Views/ConnectView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StreamingApp.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ConnectView.xaml
+    /// </summary>
+    public partial class ConnectView : UserControl
+    {
+        public ConnectView()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayVideo(string videoFilePath)
+        {
+            try
+            {
+                bg.Source = new Uri(videoFilePath);
+                bg.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            string videoFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images\\v4.mp4");
+            PlayVideo(videoFilePath);
+        }
+
+        //private void bg_MediaEnded(object sender, RoutedEventArgs e)
+        //{
+        //    bg.Position = TimeSpan.Zero;
+        //    bg.Play();
+        //}
+    }
+}
