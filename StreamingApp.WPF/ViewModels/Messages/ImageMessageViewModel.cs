b7ddc4adc751084664a0c5eac55408c6b978d649
RostyslavG <rostyslav.gritsyshen@gmail.com>
2023-12-26 10:07:26 +0200

    Added new controls and optimized styles

---
+++ StreamingApp.WPF/ViewModels/Messages/ImageMessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StreamingApp.WPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.WPF.ViewModels.Messages;
+
+class ImageMessageViewModel : ViewModelBase
+{
+}
