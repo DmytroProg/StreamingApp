dccf7a06ce5b0dc5c26e827d37ce196dbe494db5
DmytroProg <dmytroprog@gmail.com>
2024-03-20 13:25:43 +0200

    Added some features

---
+++ StreamingApp.WPF/ViewModels/EmptyMeetingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StreamingApp.BLL.Models;
+using StreamingApp.WPF.ViewModels.Base;
+
+namespace StreamingApp.WPF.ViewModels;
+
+internal class EmptyMeetingViewModel : ViewModelBase
+{
+    public Meeting Meeting { get; set; }
+
+    public EmptyMeetingViewModel(Meeting meeting)
+    {
+        Meeting = meeting;
+    }
+}
--- StreamingApp.WPF/ViewModels/RegistrationViewModel.cs
+++ StreamingApp.WPF/ViewModels/RegistrationViewModel.cs
@@ -2,6 +2,9 @@
 using StreamingApp.BLL.Models;
 using StreamingApp.WPF.Navigations;
 using StreamingApp.WPF.ViewModels.Base;
+using System;
+using System.IO;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace StreamingApp.WPF.ViewModels;
@@ -42,10 +45,27 @@ internal class RegistrationViewModel : ViewModelBase
         }
     }
 
+    public string Avatar { get; set; }
+
     public ICommand RegisterCommand { get; }
+    public ICommand SelectAvatarCommand { get; }
 
     public RegistrationViewModel() {
         RegisterCommand = new RelayCommand(Register);
+        SelectAvatarCommand = new RelayCommand(SelectAvatar);
+    }
+
+    private void SelectAvatar()
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "Image Files (JPG,PNG,GIF)|*.JPG;*.PNG;*.GIF";
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            var stream = openFileDialog.OpenFile();
+            using var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            Avatar = Convert.ToBase64String(ms.ToArray());
+        }
     }
 
     public void Register()
@@ -54,7 +74,7 @@ internal class RegistrationViewModel : ViewModelBase
             Name = Nickname,
             Login = Login,
             Password = Password,
-            AvatarImage = "img"
+            AvatarImage = Avatar ?? "0"
         };
 
         App.UnitController.UserController.Register(user);
